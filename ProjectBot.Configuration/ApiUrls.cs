b3f5cb4ee787d8fb8340ad27b66d33554e9a0a84
Modricagon <isaacarnall@gmail.com>
2019-02-06 16:15:03 +0000

    0.11

--- ProjectBot.Configuration/ApiUrls.cs
+++ ProjectBot.Configuration/ApiUrls.cs
@@ -8,5 +8,11 @@ namespace ProjectBot.Configuration
     {
         public static string opinionatedQuotesUrl { get; set; } =
             "https://opinionated-quotes-api.gigalixirapp.com/v1/quotes";
+
+        public static string randomQuotesUrl { get; } =
+            "https://sumitgohil-random-quotes-v1.p.rapidapi.com/fetch/randomQuote";
+
+        public static string randomQuotesKey { get; } =
+            "bbb8e2dff4msh79f70d5bbf04ec4p172c97jsn6f5bee234637";
     }
 }
--- ProjectBot.Domain/QuotesHelper.cs
+++ ProjectBot.Domain/QuotesHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using ProjectBot.Configuration;
@@ -12,12 +13,11 @@ namespace ProjectBot.Domain
 {
     public static class QuotesHelper
     {
-        public static string OpinionatedQuotes()
+        public static string RandomQuotes()
         {
-            var data = DownloadHelper.DownloadJObject(ApiUrls.opinionatedQuotesUrl).Result;
-
-            var jo = (JObject) data;
-            var a = jo.First.First.First;
+            Task<HttpResponse<MyClass>> response = Unirest.get("https://sumitgohil-random-quotes-v1.p.rapidapi.com/fetch/randomQuote")
+                .header("X-RapidAPI-Key", "bbb8e2dff4msh79f70d5bbf04ec4p172c97jsn6f5bee234637")
+                .asJson();
 
             string quote = "";
 
